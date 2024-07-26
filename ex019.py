409aef4a0ad18c162db5f25b5e7d64f0921da0eb
Emerson <ealima.box@gmail.com>
2024-07-26 09:41:08 -0300

    mesmos exes com anotações

--- ex019.py
+++ ex019.py
@@ -1,1 +1,8 @@
 # DESAFIO 019 - Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+al1 = str(input("Primeiro aluno: "))
+al2 = str(input("Segundo aluno: "))
+al3 = str(input("Terceiro aluno: "))
+al4 = str(input("Quarto aluno: "))
+lista = [al1, al2, al3, al4]
+print("Quem vai apagar o quadro é: {}".format(choice(lista)))
--- ex020.py
+++ ex020.py
@@ -1,1 +1,13 @@
 # DESAFIO 020- O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+al1 = str(input("Primeiro nome: "))
+al2 = str(input("Segundo nome: "))
+al3 = str(input("Terceiro nome: "))
+al4 = str(input("Quarto nome: "))
+alunos = [al1, al2, al3, al4]
+# obtive o resultado NONE ao passar: print("A ordem sorteada será {}".format(shuffle(alunos)))
+# preciso fazer o embaralhamento da lista antes de chama-la
+shuffle(alunos)
+# também não foi possível usar a string formatada, então chamei duas linhas do comando 'print'
+print("A ordem de aprensentação será: ")
+print(alunos)
