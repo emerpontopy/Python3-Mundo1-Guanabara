e98bc7b4377663344a24c7b3e8e5c1ee3b931c7d
emm3bin <ealima.box@gmail.com>
2024-10-22 20:39:35 -0300

    gerenciador de pagamentos

---
+++ ex044.py
@@ -0,0 +1,31 @@
+# DESAFIO 044 - Elabore um programaa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento: 
+# - ÀVISTA DINHEIRO/CHEQUE: 10% de desconto
+# - ÀVISTA NO CARTÃO: 5% de desconto
+# - EM ATÉ 2X NO CARTÃO: PREÇO NORMAL
+# - 3X OU MAIS NO CARTÃO: 20% DE JUROS
+print("{:=^40}".format("Lojas EmerSongs"))
+compras = float(input("Preço das compras: R$" ))
+print('''FORMAS DE PAGAMENTO
+[1] À VISTA DINHEIRO ou CHEQUE 
+[2] À VISTA CARTÃO
+[3] 2x no CARTÃO
+[4] 3x ou mais no CARTÃO''')
+formapgto = int(input("Qual é a forma de pagamento? "))
+# parcelas = int(input("Quantas parcelas? "))
+if formapgto == 1:
+    total = compras - (compras * 10 / 100)    
+elif formapgto == 2:
+    total = compras - (compras * 5 / 100)    
+elif formapgto == 3:
+    total = compras
+    vlParcela = total / 2
+    print("Sua compra será parcelada em 2x de R${:.2f}".format(vlParcela))
+elif formapgto == 4:
+    total = compras + (compras * 20 / 100)
+    parcelamento = int(input("Em quantas parcelas? "))
+    vlParcela = total / parcelamento
+    print("Sua compra será parcelada em {}x de R${:.2f} COM JUROS.".format(parcelamento, vlParcela))
+else:
+    total = compras
+    print("\033[31mOPÇÃO INVÁLIDA de pagamento\033[0;0m. Tente novamente! ")
+print("Sua compra de R${:.2f} vai custar R${:.2f} no final".format(compras, total))
